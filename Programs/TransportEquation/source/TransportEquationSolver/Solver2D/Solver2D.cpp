3a204c4e9674dc6701323240867bb1f16f33f19c
GSench <grishasenchenok@yandex.ru>
2022-02-08 17:11:22 +0300

    Solver2D

--- Programs/TransportEquation/source/TransportEquationSolver/Solver2D/Solver2D.cpp
+++ Programs/TransportEquation/source/TransportEquationSolver/Solver2D/Solver2D.cpp
@@ -4,47 +4,31 @@
 
 #include "Solver2D.h"
 #include "../Solver1D/Solver1D.h"
-#include <functional>
-#include <fstream>
+#include "Area2D.h"
+#include "VectorField2D.h"
+#include "Solver2DInstances.h"
+#include "Solver2DOutput.h"
 
 using namespace std;
-/*
-void SolveTransportEquation2D(PsyScalar scalarFunction, function<VectorField2D(int, double)> uMax, Area1D p){
-    double h = p.areaLength / p.cellCount;
-    double timeStep = p.CFL * h / p.uMax;
 
-    PsyScalar fnext = scalarFunction;
-
-    ofstream myfile;
-    myfile.open(p.resultFilePath);
-    myfile << "t=0" << endl;
-    scalarFunction.printToFile(myfile, false);
-
-    for(int n = 0; n < p.NTimeSteps; n++){
-        for(int xy = X; xy < 2; xy++){
-            function<double(double)> PsyPrev = [=](double x)->double {
-                return 0;
-            };
-
-            for(int ix=0; ix<p.cellCount; ix++){
-                for(int iy=0; iy<p.cellCount; iy++){
-                    double fi = scalarFunction.get(ix, iy);
-                    double fiPrev = scalarFunction.getWithOffset(ix, iy, -(xy==X), -(xy==Y));
-                    double fiNext = scalarFunction.getWithOffset(ix, iy, +(xy==X), +(xy==Y));
-                    double* cellBounds = (double*) malloc(sizeof(double) * 4);
-                    scalarFunction.getCellBounds(ix, iy, cellBounds);
-
-                    function<double(double)> Psy = p.FlowInterpolationFunction(fi, fiPrev, fiNext, i, p.beta, h, p.eps);
-
-                    double fiR = Psy(xR - p.uMax * timeStep / 2); //flow on right cell side is from current cell (upwind)
-                    double fiL = PsyPrev(xL - p.uMax * timeStep / 2); //flow on left cell side is from previous cell (upwind)
-                    fnext[i] = fi - p.uMax / h * (fiR - fiL) * timeStep;
-
-                    PsyPrev = Psy;
-                    double xR = (i + 1) * h;
-                }
-
-            }
+void SolveTransportEquation2D(Area2D &f,
+                              VectorField2D &u,
+                              Solver2DParams &p,
+                              Solver2DOutput &output){
+    output.print(f, 0);
+    for (int n = 0; n < p.NTimeSteps; n++) {
+        for(int y=0; y<p.cellCountY; y++){
+            f.projectX(y);
+            u.projectX(y);
+            p.projectX();
+            SolverStep(f, u, p);
         }
+        for(int x=0; x<p.cellCountX; x++){
+            f.projectY(x);
+            u.projectY(x);
+            p.projectY();
+            SolverStep(f, u, p);
+        }
+        output.print(f, n+1);
     }
-}*/
+}
--- Programs/TransportEquation/source/TransportEquationSolver/Solver2D/Solver2D.h
+++ Programs/TransportEquation/source/TransportEquationSolver/Solver2D/Solver2D.h
@@ -5,4 +5,12 @@
 #ifndef TRANSPORTEQUATION_SOLVER2D_H
 #define TRANSPORTEQUATION_SOLVER2D_H
 
+#include "Solver2DOutput.h"
+#include "VectorField2D.h"
+
+void SolveTransportEquation2D(Area2D &f,
+                              VectorField2D &u,
+                              Solver2DParams &p,
+                              Solver2DOutput &output);
+
 #endif //TRANSPORTEQUATION_SOLVER2D_H
