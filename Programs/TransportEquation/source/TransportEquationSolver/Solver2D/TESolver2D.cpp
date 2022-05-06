8ebe3e6a20113a56be6069fd6d56ba51bf40f3eb
GSench <grishasenchenok@yandex.ru>
2022-05-06 21:44:07 +0300

    TESolver2D step separated

--- Programs/TransportEquation/source/TransportEquationSolver/Solver2D/TESolver2D.cpp
+++ Programs/TransportEquation/source/TransportEquationSolver/Solver2D/TESolver2D.cpp
@@ -11,24 +11,30 @@
 
 using namespace std;
 
+void TESolver2DStep(Area2D &f,
+                    VectorField2D &u,
+                    TESolver2DParams &p){
+    for(int y=0; y<p.getCellCountY(); y++){
+        f.projectX(y);
+        u.projectX(y);
+        p.projectX();
+        TESolverStep(f, u, p);
+    }
+    for(int x=0; x<p.getCellCountX(); x++){
+        f.projectY(x);
+        u.projectY(x);
+        p.projectY();
+        TESolverStep(f, u, p);
+    }
+}
+
 void SolveTransportEquation2D(Area2D &f,
                               VectorField2D &u,
                               TESolver2DParams &p,
                               TESolver2DOutput &output){
     output.print(f, 0);
     for (int n = 0; n < p.getNTimeSteps(); n++) {
-        for(int y=0; y<p.getCellCountY(); y++){
-            f.projectX(y);
-            u.projectX(y);
-            p.projectX();
-            TESolverStep(f, u, p);
-        }
-        for(int x=0; x<p.getCellCountX(); x++){
-            f.projectY(x);
-            u.projectY(x);
-            p.projectY();
-            TESolverStep(f, u, p);
-        }
+        TESolver2DStep(f, u, p);
         output.print(f, n+1);
     }
 }
--- Programs/TransportEquation/source/TransportEquationSolver/Solver2D/TESolver2D.h
+++ Programs/TransportEquation/source/TransportEquationSolver/Solver2D/TESolver2D.h
@@ -8,6 +8,10 @@
 #include "TESolver2DOutput.h"
 #include "../Instances/VectorField2D.h"
 
+void TESolver2DStep(Area2D &f,
+                    VectorField2D &u,
+                    TESolver2DParams &p);
+
 void SolveTransportEquation2D(Area2D &f,
                               VectorField2D &u,
                               TESolver2DParams &p,
