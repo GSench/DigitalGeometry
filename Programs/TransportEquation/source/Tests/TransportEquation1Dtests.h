66d62b57fbf4c1b79ee196922b8bd05578b6e7f2
GSench <grishasenchenok@yandex.ru>
2022-12-05 17:26:30 +0300

    Tests init work

--- Programs/TransportEquation/source/Tests/TransportEquation1Dtests.h
+++ Programs/TransportEquation/source/Tests/TransportEquation1Dtests.h
@@ -2,9 +2,10 @@
 // Created by GSench on 14.12.2021.
 //
 
+void TE1DStripMovementTest();
+/*
 void Solver1DStripMovementTest();
 void Solver1DStripBackMovementTest();
 void Solver1DJRStripMovementTest();
 void Solver1Dtests();
-bool test1DSolverStandard();
-bool test1DSolverBackStandard();
+ */
